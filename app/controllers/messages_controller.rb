5b004271da453757d4ec2ebe831662fe4c6ff549
suminofu14 <red.chili.pepper14@gmail.com>
2019-11-24 19:42:54 +0900

    コントローラー記述

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,8 +1,19 @@
 class MessagesController < ApplicationController
   def index
+    @messages = Message.all
   end
 
   def create
+    @message = Message.create(message_params)
+    if @message.save
+      redirect_to root_path
+    else
+      render :index, alert: 'メッセージを入力してください。'
+    end
   end
 
+  private
+  def message_params
+    params.require(:message).permit(:body, :image)
+  end
 end
