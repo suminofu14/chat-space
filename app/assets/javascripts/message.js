2ceca4e85cd7f60ed57d6e056eede9c7cecba4d4
suminofu14 <red.chili.pepper14@gmail.com>
2019-12-05 19:40:28 +0900

    jbuilder実装

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,3 +1,22 @@
 $(function() {
-  console.log("OK");
+  $('#new_message').on('submit', function(e){
+    e.preventDefault();
+    console.log(this);
+    var formData = new FormData(this);
+    var url = $(this).attr('action');
+    $.ajax({
+      url: url,
+      type: "POST",
+      data: formData,
+      dataType: 'json',
+      processData: false,
+      contentType: false
+    })
+    .done(function(message){
+      console.log(message);
+    })
+    .fail(function(){
+
+    })
+  })
 });
---
+++ app/views/messages/create.json.jbuilder
@@ -0,0 +1,3 @@
+json.body @message.body
+json.image @message.image
+json.date @message.created_at
