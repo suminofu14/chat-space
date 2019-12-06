810adfddcc68790c7c0c5b41fd1f6bf0aacb1306
suminofu14 <red.chili.pepper14@gmail.com>
2019-12-06 13:03:37 +0900

    投稿成功時の非同期化

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,7 +1,46 @@
 $(function() {
+
+  function buildMessage(message){
+    if (message.image) {
+      var image = message.image.file.match(/\/uploads.+/)
+      var html = `<div class="message">
+                    <div class="message__upper-info">
+                      <div class="message__upper-info__talker">
+                        ${message.name}
+                      </div>
+                      <div class="message__upper-info__date">
+                        ${message.date}
+                      </div>
+                    </div>
+                    <div class="message__text">
+                      <p class="lower-messge__content">
+                        ${message.body}
+                      </p>
+                      <img src="${image}">
+                    </div>
+                  </div>`
+    } else {
+      var html = `<div class="message">
+                    <div class="message__upper-info">
+                      <div class="message__upper-info__talker">
+                        ${message.name}
+                      </div>
+                      <div class="message__upper-info__date">
+                        ${message.date}
+                      </div>
+                    </div>
+                    <div class="message__text">
+                      <p class="lower-messge__content">
+                        ${message.body}
+                      </p>
+                    </div>
+                  </div>`
+    }
+    return html;
+  }
+
   $('#new_message').on('submit', function(e){
     e.preventDefault();
-    console.log(this);
     var formData = new FormData(this);
     var url = $(this).attr('action');
     $.ajax({
@@ -13,10 +52,16 @@ $(function() {
       contentType: false
     })
     .done(function(message){
-      console.log(message);
+      var html = buildMessage(message);
+      $('.messages').append(html);
+      $('.messages').animate({ scrollTop: $('.messages')[0].scrollHeight});
+      $('#new_message')[0].reset();
     })
     .fail(function(){
 
+    })
+    .always(() => {
+      $('.submit-btn').removeAttr("disabled")
     })
   })
 });
--- app/views/messages/create.json.jbuilder
+++ app/views/messages/create.json.jbuilder
@@ -1,3 +1,4 @@
 json.body @message.body
-json.image @message.image
+json.image @message.image.file
 json.date @message.created_at
+json.name @message.user.name
