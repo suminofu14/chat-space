d5cd9d8356c490ec0c0a93f3fb88eec7f97d697b
suminofu14 <red.chili.pepper14@gmail.com>
2019-12-08 17:56:13 +0900

    テスト実行

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,7 @@ class UsersController < ApplicationController
   def index
     @users = User.search(params[:keyword], current_user.id)
     respond_to do |format|
-      #format.html
+      format.html
       format.json
     end
   end
