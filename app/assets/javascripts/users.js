8884809505d2b64309a74f926ef63c1a75fbe56e
suminofu14 <red.chili.pepper14@gmail.com>
2019-12-08 17:55:20 +0900

    現在のメンバーに追加

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -18,6 +18,21 @@ $(function() {
     $("#user-search-result").append(html);
   }
 
+  function addDeleteUser(name, id) {
+    let html = `
+                <div class='chat-group-user clearfix' id="${id}">
+                  <p class='chat-group-user__name'>${name}</p>
+                  <div class='user-search-remove chat-group-user__btn chat-group-user__btn--remove js-remove-btn' data-user-id="${id}" data-user-name="${name}">削除</div>
+                </div>
+                `;
+    $(".js-add-user").append(html);
+  }
+
+  function addMember(userId) {
+    let html = `<input value="${userId}" name="group[user_ids][]" type="hidden" id="group_user_ids_${userId}" />`;
+    $(`#${userId}`).append(html);
+  }
+
   $("#user-search-field").on("keyup", function() {
     var input = $("#user-search-field").val();
     $.ajax({
@@ -44,9 +59,15 @@ $(function() {
     })
   });
 
-  $(".chat-group-form__field--right").on("click", ".chat-group-user__btn--add", function(){
+  $(document).on("click", ".chat-group-user__btn--add", function(){
     const userName = $(this).attr("data-user-name");
     const userId = $(this).attr("data-user-id");
+    $(this).parent().remove();
+    addDeleteUser(userName, userId);
+    addMember(userId);
+  });
+
+  $(document).on("click", ".chat-group-user__btn--remove", function() {
     $(this).parent().remove();
   });
 });
